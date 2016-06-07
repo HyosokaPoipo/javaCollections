410c15fbf8147982b8c17622f5c247985a79e4a4
Maryadi <maryadi@elephanttalk.com>
2016-06-07 09:14:14 +0700

    nambain sort, reverse, binarysearch...

--- src/hisoka/poipo/MainClass.java
+++ src/hisoka/poipo/MainClass.java
@@ -1,17 +1,28 @@
 package hisoka.poipo;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		/* ArrayList */
-		List arrList = new ArrayList();
-		arrList.add(3); arrList.add(5); arrList.add(7); arrList.add(2); arrList.add("test");
-		System.out.println(arrList);
+		List<Integer> arrList = new ArrayList<Integer>();		
+		arrList.add(3); 
+		arrList.add(5); 
+		arrList.add(7);
+		arrList.add(2); 
 		
-
+		System.out.println("Hasil sebelum disort " +arrList);
+		Collections.sort(arrList);
+		System.out.println("Hasil setelah disort " + arrList);
+		Collections.reverse(arrList);
+		System.out.println("Hasil setelah direverse " + arrList);
+		
+		/* binary search */
+		int indeks = Collections.binarySearch(arrList, 7);
+		System.out.println("Indeks : "+ indeks);
 	}
 
 }
