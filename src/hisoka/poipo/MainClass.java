f4e53131e1292f1e09add6fd940709fa87635429
Maryadi <maryadi@elephanttalk.com>
2016-06-07 08:20:07 +0700

    Nambain List  - ArrayList

--- src/hisoka/poipo/MainClass.java
+++ src/hisoka/poipo/MainClass.java
@@ -1,9 +1,16 @@
 package hisoka.poipo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* ArrayList */
+		List arrList = new ArrayList();
+		arrList.add(3); arrList.add(5); arrList.add(7); arrList.add(2); arrList.add("test");
+		System.out.println(arrList);
+		
 
 	}
 
